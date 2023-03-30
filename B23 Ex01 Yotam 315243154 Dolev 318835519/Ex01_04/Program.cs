da0bf3ce8f8422615b46bc08a7cda7364e3bfe22
YotamGuetta <yotamguetta1@gmail.com>
2023-03-30 18:59:19 +0300

    Ex01_04-05

--- B23 Ex01 Yotam 315243154 Dolev 318835519/Ex01_04/Program.cs
+++ B23 Ex01 Yotam 315243154 Dolev 318835519/Ex01_04/Program.cs
@@ -1,83 +1,135 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex01_04
+﻿namespace Ex01_04
 {
-    class Program
-    {
+    using System;
+
+    public class Program
+    {   
+        private const int k_DesiredLength = 6;
+        private const bool k_ValidStringFormat = true;
         private static int m_StringAsANumber;
         private static string m_EnglishCharactersString;
-        private const int k_DesiredLength = 6;
-        private const bool k_validString = true;
-        private static bool m_isStringOfNumbers;
-        private static bool m_isStringOfEnglishCharacters;
+        private static bool m_IsAStringOfNumbers = false;
 
-        private static bool isValidInputLength(string i_StringToCheck)
-        {
-            return k_DesiredLength == i_StringToCheck.Length;
-        }
-        private static bool IsValidInputCharecters(string i_StringToCheck)
-        {
-            m_isStringOfNumbers = int.TryParse(i_StringToCheck, out m_StringAsANumber);
-            if (m_isStringOfNumbers) ;
-            for (int i = 0; i < i_StringToCheck.Length; i++)
-            {
-                if (!IsEnglishLetter(i_StringToCheck[i]))
-                {
-                    return !k_validString;
-                }
-            }
-
-            return k_validString;
-        }
-
-        private static bool IsEnglishLetter(char i_Letter)
-        {
-            return (i_Letter >= 'a' && i_Letter <= 'z') || (i_Letter >= 'A' && i_Letter <= 'Z');
-        }
-
-        private static bool IsValidInputString(string i_StringInput)
-        {
-            return isValidInputLength(i_StringInput) && IsValidInputCharecters(i_StringInput);
-
-        }
-
-
-        public bool IsPolindrom()
-        {
-            return true;
-        }
-
-        public bool IsDividableBy3()
-        {
-            return true;
-        }
-
-        public bool UpperCaseLettersSum()
-        {
-            return true;
-        }
-
-        
         public static void Main()
         {
-           
-
             string userInput = Console.ReadLine();
 
-
-
-            if (!IsValidInputString(userInput))
+            if (!InsertStringToDataMembers(userInput))
             {
                 Console.WriteLine("Invalid input");
             }
             else
             {
+                if (IsPalindrome())
+                {
+                    Console.WriteLine("The string is a palindrome");
+                }
+                else
+                {
+                    Console.WriteLine("The string is not a palindrome");
+                }
 
+                if (m_IsAStringOfNumbers)
+                {
+                    if (IsDividableBy3())
+                    {
+                        Console.WriteLine("The Number is Dividable by 3");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Number is not Dividable by 3");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format(@"There are {0} upper case letters in the string", UpperCaseLettersSum()));
+                }
             }
         }
+
+        private static bool IsValidInputLength(string i_StringToCheck)
+        {
+            return k_DesiredLength == i_StringToCheck.Length;
+        }
+
+        private static bool InsertStringToDataMembers(string i_StringToAdd)
+        {
+            if (!IsValidInputLength(i_StringToAdd))
+            {
+                return !k_ValidStringFormat;
+            }
+
+            m_EnglishCharactersString = i_StringToAdd;
+
+            m_IsAStringOfNumbers = int.TryParse(i_StringToAdd, out m_StringAsANumber);
+
+            if (!m_IsAStringOfNumbers)
+            {
+                for (int i = 0; i < i_StringToAdd.Length; i++)
+                {
+                    if (!IsEnglishLetter(i_StringToAdd[i]))
+                    {
+                        return !k_ValidStringFormat;
+                    }
+                }
+            }
+
+            return k_ValidStringFormat;
+        }
+
+        private static bool IsEnglishLetter(char i_Letter)
+        {
+            return (i_Letter >= 'a' && i_Letter <= 'z') || IsUpperCaseLetter(i_Letter);
+        }
+
+        private static bool IsPalindrome()
+        {
+            return IsPalindromeRecursion(0, m_EnglishCharactersString.Length - 1);
+        }
+
+        private static bool IsPalindromeRecursion(int i_StringStartChar, int i_StringEndChar)
+        {
+            if (i_StringStartChar == i_StringEndChar)
+            {
+                return true;
+            }   
+
+            if (m_EnglishCharactersString[i_StringStartChar] != m_EnglishCharactersString[i_StringEndChar])
+            {
+                return false;
+            }
+                
+            if (i_StringStartChar < i_StringEndChar + 1)
+            {
+                return IsPalindromeRecursion(i_StringStartChar + 1, i_StringEndChar - 1);
+            }
+                
+            return true;
+        }
+
+        private static bool IsDividableBy3()
+        {
+            return m_StringAsANumber % 3 == 0;
+        }
+
+        private static int UpperCaseLettersSum()
+        {
+            int numberOfUpperCaseLetters = 0;
+
+            for (int i = 0; i < m_EnglishCharactersString.Length; i++)
+            {
+                if (IsUpperCaseLetter(m_EnglishCharactersString[i]))
+                {
+                    numberOfUpperCaseLetters++;
+                }
+            }
+
+            return numberOfUpperCaseLetters;
+        }
+
+        private static bool IsUpperCaseLetter(char i_Letter)
+        {
+            return i_Letter >= 'A' && i_Letter <= 'Z';
+        }     
     }
 }
--- B23 Ex01 Yotam 315243154 Dolev 318835519/Ex01_05/Program.cs
+++ B23 Ex01 Yotam 315243154 Dolev 318835519/Ex01_05/Program.cs
@@ -1,13 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex01_05
+﻿namespace Ex01_05
 {
-    class Program
-    {
+    using System;
 
+    public class Program
+    {
+        private const int k_DesiredLength = 6;
+        private const bool k_ValidStringFormat = true;
+        private static int m_StringAsANumber;
+        
+        public static void Main()
+        {
+            while (true)
+            {
+                string userInput;
+                do
+                {
+                    userInput = Console.ReadLine();
+                }
+                while (!InsertStringToDataMembers(userInput));
+
+                Console.WriteLine(string.Format(
+@"The number of digits bigger then the unit's place is: {0}
+The minimum digit is: {1}
+The number of digits dividable by 3 are: {2}
+The avrage of the digits is: {3}",
+NumberOfDigitsBiggerThenTheUnitsPlace(),
+MinimumDigit(),
+NumberOfDigitsDividableBy3(),
+DigitsAvrage()));
+            }
+        }
+
+        private static int NumberOfDigitsBiggerThenTheUnitsPlace()
+        {
+            short digitToCompare = TheNumbersDigit(0);
+            int numberOfBiggerDigits = 0;
+            for (int i = 1; i < k_DesiredLength; i++)
+            {
+                if (TheNumbersDigit(i) > digitToCompare)
+                {
+                    numberOfBiggerDigits++;
+                }
+            }
+
+            return numberOfBiggerDigits;
+        }
+
+        private static int MinimumDigit()
+        {
+            int minimumDigit = 1;
+            short currentDigitValue;
+            short minimumDigitValue = TheNumbersDigit(minimumDigit - 1);
+            for (int i = 1; i < k_DesiredLength; i++)
+            {
+                currentDigitValue = TheNumbersDigit(i);
+                if (currentDigitValue < minimumDigitValue)
+                {
+                    minimumDigitValue = currentDigitValue;
+                    minimumDigit = i + 1;
+                }
+            }
+
+            return minimumDigit;
+        }
+
+        private static int NumberOfDigitsDividableBy3()
+        {
+            int numberOfDigitsDividableBy3 = 0;
+            for (int i = 0; i < k_DesiredLength; i++)
+            {
+                if (TheNumbersDigit(i) % 3 == 0)
+                {
+                    numberOfDigitsDividableBy3++;
+                }
+            }
+
+            return numberOfDigitsDividableBy3;
+        }
+
+        private static float DigitsAvrage()
+        {
+            int sumOfDigits = 0;
+            int numberOfDigits = 0;
+            for (int i = 0; i < k_DesiredLength; i++)
+            {
+                sumOfDigits += TheNumbersDigit(i);
+
+                numberOfDigits++;
+            }
+
+            return (float)sumOfDigits / numberOfDigits;
+        }
+
+        private static short TheNumbersDigit(int i_DigitIndex)
+        {
+            return (short)((m_StringAsANumber % Math.Pow(10.0, i_DigitIndex + 1)) / Math.Pow(10, i_DigitIndex));
+        }
+
+        private static bool IsValidInputLength(string i_StringToCheck)
+        {
+            return k_DesiredLength == i_StringToCheck.Length;
+        }
+
+        private static bool InsertStringToDataMembers(string i_StringToAdd)
+        {
+            if (!IsValidInputLength(i_StringToAdd))
+            {
+                Console.WriteLine("Invalid input");
+                return !k_ValidStringFormat;
+            }
+
+           if(int.TryParse(i_StringToAdd, out m_StringAsANumber))
+            {
+                return k_ValidStringFormat;
+            }
+
+            Console.WriteLine("Invalid input");
+            return !k_ValidStringFormat;
+        }
     }
 }
