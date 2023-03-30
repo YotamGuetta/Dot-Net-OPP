f7d04cac26c6704d2192a73a565bc7879f48bdbb
DolevAvior <dolev98@gmail.com>
2023-03-30 19:54:54 +0300

    Done Ex01_02

--- B23 Ex01 Yotam 315243154 Dolev 318835519/Ex01_02/Program.cs
+++ B23 Ex01 Yotam 315243154 Dolev 318835519/Ex01_02/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex01_02
 {
     class Program
     {
+        static void Main()
+        {
+            StringBuilder SB = new StringBuilder();
+            printDiamondForBegginers(SB, 5, 0);
+            Console.WriteLine(SB.ToString());
+        }
+
+        static void printDiamondForBegginers(StringBuilder i_SB, int i_numOfRows, int i_currenRow)
+        {
+            if (i_numOfRows < 1)
+            {
+                return;
+            }
+
+            i_SB.Append(' ', i_numOfRows - 1);
+            i_SB.Append('*', 2 * i_currenRow + 1);
+            i_SB.AppendLine();
+            printDiamond(sb, i_numOfRows - 1, i_currenRow + 1);
+            if (i_currenRow != 0)
+            {
+                i_SB.Append(' ', i_numOfRows);
+                i_SB.Append('*', (2 * i_currenRow) - 1);
+                i_SB.AppendLine();
+            }
+        }
     }
+}
 }
