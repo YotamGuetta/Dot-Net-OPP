2c5dd0792209b05a8eb936e7afa4936758110950
YotamGuetta <yotamguetta1@gmail.com>
2023-05-25 17:48:54 +0300

    Ex-3 Finale

---
+++ B23 Ex03 Yotam 315243154 Dolev 318835519/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UI v_ui = new UI();
+            v_ui.Menu();
+        }
+    }
+}
---
+++ B23 Ex03 Yotam 315243154 Dolev 318835519/Ex03.GarageLogic/TestProgram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace GarageLogic
+{
+    class TestProgram
+    {
+        enum eNumber
+        {
+            num,
+            m
+        }
+        
+        static void Main() 
+        {
+            Type objectTypeEnum = typeof(eNumber);
+            Type objectType = typeof(eNumber);
+            bool isEnum;
+            isEnum = objectType.IsEnum;
+            if (objectType.Equals(typeof(string)))
+            {
+                Console.WriteLine("Enter a value to parse: ");
+                string inputValue = Console.ReadLine();
+                //object enumObject = inputValue;
+            }
+            if (isEnum) {
+                objectType = typeof(int);
+            }
+
+            MethodInfo parseMethod = objectType.GetMethod("Parse", new[] { typeof(string) });
+
+            if (parseMethod != null)
+            {
+                Console.WriteLine("Enter a value to parse: ");
+                string inputValue = Console.ReadLine();
+
+                try
+                {
+                    object parsedValue = parseMethod.Invoke(null, new object[] { inputValue });
+                    
+                    if (isEnum)
+                    {
+                        int enumValue = (int)parsedValue;
+                        object enumObject = Enum.ToObject(objectTypeEnum, enumValue);
+                        eNumber convertedEnum = (eNumber)enumObject;
+
+                        Console.WriteLine("Converted enum value: " + convertedEnum);
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Parsed value: " + parsedValue);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            
+
+            // Type type = stuff.GetType();
+            //MethodInfo method = type.GetMethod("Parse");
+            //method.Invoke()
+            //Console.WriteLine(method.Name);
+        }
+    }
+}
